956781e22c7c37ac9cd35014a9db14f8566e5744
Scenaristeur <scenaristeur@gmail.com>
2021-06-30 19:43:52 +0200

    butt +aframe 1.2.0

--- script.js
+++ script.js
@@ -1,6 +1,65 @@
-// getting places from APIs
+window.onload = () => {
 
-function loadPlaces(position){
+  document.querySelector(".say-hi-button")
+  .addEventListener("click", function () {
+    // here you can change also a-scene or a-entity properties, like
+    // changing your 3D model source, size, position and so on
+    // or you can just open links, trigger actions...
+    alert("Hi there!");
+  });
+
+  let method = 'dynamic';
+
+  // if you want to statically add places, de-comment following line
+  //method = 'static';
+
+  if (method === 'static') {
+    let places = staticLoadPlaces();
+    renderPlaces(places);
+  }
+
+  if (method !== 'static') {
+
+    // first get current user location
+    return navigator.geolocation.getCurrentPosition(function (position) {
+
+      // than use it to load from remote APIs some places nearby
+      dynamicLoadPlaces(position.coords)
+      .then((places) => {
+        renderPlaces(places);
+      })
+    },
+    (err) => console.error('Error in retrieving position', err),
+    {
+      enableHighAccuracy: true,
+      maximumAge: 0,
+      timeout: 27000,
+    }
+  );
+}
+};
+
+function staticLoadPlaces() {
+  return [
+    {
+      name: "Your place name",
+      location: {
+        lat: 0, // add here latitude if using static data
+        lng: 0, // add here longitude if using static data
+      }
+    },
+    {
+      name: 'Another place name',
+      location: {
+        lat: 0,
+        lng: 0,
+      }
+    }
+  ];
+}
+
+// getting places from REST APIs
+function dynamicLoadPlaces(position){
   let lat = position.latitude
   let lon = position.longitude
   console.log(lat,lon)
@@ -31,127 +90,53 @@ function loadPlaces(position){
     console.log(resp)
     return resp.query.geosearch;
   })
-
-  // .then(function(response) {
-  //   console.log(response)
-  //   var res = response.query.geosearch;
-  //   console.log(res)
-  //   for (var r in res) {
-  //     let thing = res[r]
-  //     console.info(thing)
-  //     console.log(thing.lat, thing.lon, thing.dist)
-  //
-  //     // console.log("Latitute: " + res[r].coordinates[0].lat);
-  //     // console.log("Longitude: " + res[r].coordinates[0].lon);
-  //   }
-  // })
   .catch(function(error){console.log(error);});
 
 }
 
+function renderPlaces(places) {
+  let scene = document.querySelector('a-scene');
 
-window.onload = () => {
+  places.forEach((place) => {
+    console.log(place)
+    const latitude = place.lat;
+    const longitude = place.lon;
 
-  document
-         .querySelector(".say-hi-button")
-         .addEventListener("click", function () {
-           // here you can change also a-scene or a-entity properties, like
-           // changing your 3D model source, size, position and so on
-           // or you can just open links, trigger actions...
-           alert("Hi there!");
-         });
+    // add place icon
+    const icon = document.createElement('a-image');
+    icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+    icon.setAttribute('name', place.title);
+    icon.setAttribute('src', 'assets/map-marker.png');
 
+    // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+    icon.setAttribute('scale', '20, 20');
 
-  const scene = document.querySelector('a-scene');
+    icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
 
-  // first get current user location
-  return navigator.geolocation.getCurrentPosition(function (position) {
+    const clickListener = function (ev) {
+      ev.stopPropagation();
+      ev.preventDefault();
 
-    // than use it to load from remote APIs some places nearby
-    loadPlaces(position.coords)
-    .then((places) => {
-      console.log(places)
-      places.forEach((place) => {
-        const latitude = place.lat;
-        const longitude = place.lon;
+      const name = ev.target.getAttribute('name');
 
-        // add place name
-        const placeText = document.createElement('a-link');
-        placeText.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        placeText.setAttribute('title', place.title+ " "+place.dist+"m");
-        placeText.setAttribute('scale', '30 15 15');
+      const el = ev.detail.intersection && ev.detail.intersection.object.el;
 
-        placeText.addEventListener('loaded', () => {
-          window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
+      if (el && el === ev.target) {
+        const label = document.createElement('span');
+        const container = document.createElement('div');
+        container.setAttribute('id', 'place-label');
+        label.innerText = name;
+        container.appendChild(label);
+        document.body.appendChild(container);
 
+        setTimeout(() => {
+          container.parentElement.removeChild(container);
+        }, 1500);
+      }
+    };
 
-        const clickListener = function (ev) {
-          ev.stopPropagation();
-          ev.preventDefault();
-          alert(ev)
+    icon.addEventListener('click', clickListener);
 
-          const name = ev.target.getAttribute('title');
-
-          const el = ev.detail.intersection && ev.detail.intersection.object.el;
-
-          if (el && el === ev.target) {
-            const label = document.createElement('span');
-            const container = document.createElement('div');
-            container.setAttribute('id', 'place-label');
-            label.innerText = name;
-            container.appendChild(label);
-            document.body.appendChild(container);
-
-            setTimeout(() => {
-              container.parentElement.removeChild(container);
-            }, 1500);
-          }
-        };
-
-        placeText.addEventListener('click', clickListener);
-
-        scene.appendChild(placeText);
-      });
-    })
-  },
-  (err) => console.error('Error in retrieving position', err),
-  {
-    enableHighAccuracy: true,
-    maximumAge: 0,
-    timeout: 27000,
-  }
-);
-};
-
-
-function loadPlaces1(position) {
-  const params = {
-    radius: 300,    // search places not farther than this value (in meters)
-    clientId: 'client-id',
-    clientSecret: 'secret',
-    version: '20300101',    // foursquare versioning, required but unuseful for this demo
-  };
-
-  // CORS Proxy to avoid CORS problems
-  const corsProxy = 'https://cors-anywhere.herokuapp.com/';
-
-  // Foursquare API (limit param: number of maximum places to fetch)
-  const endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
-  &ll=${position.latitude},${position.longitude}
-  &radius=${params.radius}
-  &client_id=${params.clientId}
-  &client_secret=${params.clientSecret}
-  &limit=30
-  &v=${params.version}`;
-  return fetch(endpoint)
-  .then((res) => {
-    return res.json()
-    .then((resp) => {
-      return resp.response.venues;
-    })
-  })
-  .catch((err) => {
-    console.error('Error with places API', err);
-  })
-};
+    scene.appendChild(icon);
+  });
+}
